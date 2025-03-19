86da967d8e4d9c10a2afb8848b54d7ad3b55a47e
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-03-20 00:32:42 +0500

    changes made

--- sanity_integration/src/app/page.tsx
+++ sanity_integration/src/app/page.tsx
@@ -1,12 +1,12 @@
-// import RedirectToStudio from "@/components/RedirectToStudio";
+import RedirectToStudio from "@/components/RedirectToStudio";
 
-import { getProducts } from "./api/products";
+// import { getProducts } from "./api/products/route";
 
 export default async function Home() {
-  const Data = await getProducts();
-  console.log(Data);
-  // return <RedirectToStudio />;
-  return(
-    <></>
-  )
+  // const Data = await getProducts();
+  // console.log(Data);
+  return <RedirectToStudio />;
+  // return(
+  //   <></>
+  // )
 }
