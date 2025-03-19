5f40ba69033c3f41e956803664d0d179ca90cea0
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-03-20 00:39:33 +0500

    route.ts

--- sanity_integration/src/app/api/products/route.ts
+++ sanity_integration/src/app/api/products/route.ts
@@ -1,15 +1,17 @@
 import { client } from "@/sanity/lib/client";
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-export function getProducts() {
-    const Data = client.fetch('*[_type == "product"]');
-    return Data
+// Fetch products from Sanity (ensure async)
+export async function getProducts() {
+    const data = await client.fetch('*[_type == "product"]');
+    return data; // Returning resolved data
 }
 
+// API GET route
 export async function GET() {
   const data = {
     message: "Hello from products storage!",
-    items: await getProducts(),
+    items: await getProducts(), // Await the function to resolve
   };
 
   return NextResponse.json(data, { status: 200 });
