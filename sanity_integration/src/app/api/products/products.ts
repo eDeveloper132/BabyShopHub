b1a9ea15dacd4bff77556a8a93acf2b5358a7fab
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-03-19 23:53:38 +0500

    added product api

--- sanity_integration/src/app/api/products/products.ts
+++ sanity_integration/src/app/api/products/products.ts
@@ -1,5 +1,16 @@
 import { client } from "@/sanity/lib/client";
+import { NextResponse } from 'next/server';
+
 export function getProducts() {
     const Data = client.fetch('*[_type == "product"]');
     return Data
+}
+
+export async function GET() {
+  const data = {
+    message: "Hello from products storage!",
+    items: await getProducts(),
+  };
+
+  return NextResponse.json(data, { status: 200 });
 }
