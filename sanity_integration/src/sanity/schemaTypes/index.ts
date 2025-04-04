dcd18f421183a51fb62bad115ed584f294f38fe4
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-04-05 02:52:42 +0500

    ab mila

--- sanity_integration/src/sanity/schemaTypes/index.ts
+++ sanity_integration/src/sanity/schemaTypes/index.ts
@@ -1,10 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import shoesSchema from '../shoes'
 import profileSchema from '../profile'
 import innerwearsSchema from '../innerwears'
 import outfitsSchema from '../outfits'
 import toysSchema from '../toys'
 import watchesSchema from '../watches'
+import shoesSchema from '../shoes'
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [shoesSchema, innerwearsSchema, outfitsSchema, toysSchema, watchesSchema, profileSchema],
 }
